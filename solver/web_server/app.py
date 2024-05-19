37908b1544f43c11ef6c028cf1f89272670a613a
EncryptEx <limusina10@gmail.com>
2024-05-19 06:47:54 +0200

    feat: :fire: Added a flask function to recieve feedback data

--- solver/web_server/app.py
+++ solver/web_server/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,16 +8,29 @@ app = Flask(__name__)
 def readFile():
     # read file
     # return file content
-    with open('solucio.json', 'r') as file:
+    with open("solucio.json", "r") as file:
         data = json.load(file)
     return jsonify(data)
 
 
-
-
-@app.route("/generate")
+@app.route("/generate", methods=["POST"])
 def generate():
-    pass
+    global feedback_responses
+    try:
+        # Get the JSON data from the request
+        data = request.get_json()
+        
+        # Store the JSON data in the feedback_responses variable
+        # HERE YOU HAVE THE DATA:
+        feedback_responses = data
+        
+        # Print the feedback_responses variable to verify
+        # print(json.dumps(feedback_responses, indent=4))
+        # print(feedback_responses['data'][0]['municipi'])
+        
+        return jsonify({'message': 'Data received successfully!'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Failed to receive data', 'error': str(e)}), 400
 
 
 if __name__ == "__main__":
